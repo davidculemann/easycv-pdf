dbb28174fa62f5a86cb3612e6f01cf61c9e53966
David Culemann <davidculemann@gmail.com>
2025-05-18 19:50:54 +0100

    update render endpoint to match easycv

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
 	res.status(403).json({ error: 'Forbidden' });
 });
 
-app.post("/render", async (req, res) => {
+app.post("/generate-thumbnail", async (req, res) => {
 	try {
 		const pdfData = req.body;
 		const loadingTask = pdfjsLib.getDocument({ data: pdfData });
